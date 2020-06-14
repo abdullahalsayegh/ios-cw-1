be6a59dd4008b887be8c1fea7456ad33f8312acf
abdullah <Ab.alsayegh2004@gmail.com>
2020-06-14 16:38:04 +0300

    done from cw-1

---
+++ ggg/MyPlayground.playground/Contents.swift
@@ -0,0 +1,65 @@
+import UIKit
+
+//var : متغير
+//str مكان للتخزين
+var str = "Hello, playground"
+
+
+let name = "abdullah alsayegh"
+var height = 170
+var age = 15
+var hobby1 =  "football"
+var hobby2 =  "SWIMMING"
+
+print( "my name is: \(name), and my age is \(age), and my height is \(height), and my hobby1 is \(hobby1), \(hobby2)")
+
+
+let friendName = "abdalaziz"
+var friendHeigh =  175
+var friendAge =  16
+var friendHobby1 = "football"
+var friendHobby2 = "cooking"
+ 
+var age_difference = friendAge - age
+
+//age comparison
+age < friendAge
+age > friendAge
+age >= friendAge
+age <= friendAge
+age == friendAge
+age != friendAge
+
+//friendhobby1 comparison
+friendHobby1 < hobby1
+friendHobby1 > hobby1
+friendHobby1 >= hobby1
+friendHobby1 <= hobby1
+friendHobby1 == hobby1
+friendHobby1 != hobby1
+
+//friendhobby2 comparison
+friendHobby2 < hobby2
+friendHobby2 > hobby2
+friendHobby2 >= hobby2
+friendHobby2 <= hobby2
+friendHobby2 == hobby2
+friendHobby2 != hobby2
+
+
+//hieght comparison
+friendHeigh < height
+friendHeigh > height
+friendHeigh >= height
+friendHeigh <= height
+friendHeigh == height
+friendHeigh != height
+
+
+
+
+
+
+
+
+
